bdfa0dfabdcdd5d456a94fb9fbae278c704f057d
Anner Henriquez <annerh3@gmail.com>
2025-08-30 11:24:44 -0600

    feat: update card component

--- src/shared/components/ui/Card.tsx
+++ src/shared/components/ui/Card.tsx
@@ -4,7 +4,7 @@ export const Card = () => {
   return (
     <main className="mt-10">
       <text className="p-10 text-xl rounded-lg carbon-theme">
-        Ever gay
+        Ever gay gay
         <DeliveryParcel size={32} className="inline-block ml-2 mb-1" />
       </text>
     </main>
